af7d3322935dacbfaed4248740c08a80feacb49b
darshanasam1234 <64640091+darshanasam1234@users.noreply.github.com>
2023-09-08 00:30:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
-    const button = document.getElementById("changeText");
-    const main = document.querySelector("main p");
+    const changeTextButton = document.getElementById("changeText");
+    const changeBackgroundColorButton = document.getElementById("changeBackgroundColor");
+    const main = document.querySelector("main");
+    const text = document.querySelector("main p");
 
-    button.addEventListener("click", function () {
-        main.textContent = "Text changed!";
+    changeTextButton.addEventListener("click", function () {
+        text.textContent = "Text changed!";
+    });
+
+    changeBackgroundColorButton.addEventListener("click", function () {
+        // Generate a random color (hexadecimal)
+        const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        main.style.backgroundColor = randomColor;
     });
 });
